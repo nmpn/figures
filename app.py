085d0351a9fd0eb2c16a9a5d3ee73d6cda3bf025
Saisakul Chernbumroong <42466157+nmpn@users.noreply.github.com>
2024-07-26 20:01:19 +0100

    Add drop down. Work with sub data frame

--- app.py
+++ app.py
@@ -14,6 +14,13 @@ def init_state():
     if 'checkbox_indices' not in st.session_state:
         st.session_state.checkbox_indices = {}
 
+
+def sort_by_size(scope, desc = True):
+    # Group by 'B', get unique values from 'A' and convert to list
+    result = st.session_state.all_data[st.session_state.all_data.C == scope].groupby('B')['A'].unique().apply(list).to_dict()
+    st.session_state.my_data = dict(sorted(result.items(), key=lambda item: len(item[1]), reverse=desc))
+    st.session_state.length = len(st.session_state.my_data)
+
 # Load data
 def load_data():
     df = pd.DataFrame({
@@ -21,36 +28,58 @@ def load_data():
               'Manually tested', 'Hello World programmer', 'Eight by nine', 'A text is a must', 'That will be all',
               'Sixteen twenty one', 'All of things come to an end', 'Good bye all',
               'I will rise again', 'Until now so long'],
-        'B': ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'd', 'e', 'e', 'e', 'e', 'e']
+        'B': ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c', 'd', 'e', 'e', 'e', 'e', 'e'],
+        'C': ['In', 'In', 'Out', 'In','In','In', 'Out', 'Out', 'Out', 'Out', 'In', 'In', 'Out', 'Out', 'Out']
     })
-    st.session_state.my_data = df.groupby('B').A.unique().apply(list).to_dict()
-    st.session_state.length = len(st.session_state.my_data)
+    st.session_state.all_data = df
+
 
 def expander_content(key, alist):
     if key not in st.session_state.checkbox_indices:
         st.session_state.checkbox_indices[key] = 0
 
     index = st.session_state.checkbox_indices[key]
-    sublist = alist[:index + 2]
+    sublist = alist[:index + max_checkbox]
 
     for item in sublist:
         st.checkbox(item, key=f'{key}_{item}')
 
-    if index + 2 < len(alist):
+    if index + max_checkbox < len(alist):
         if st.button('Load more checkboxes', key=f'{key}_load_more_checkboxes'):
-            st.session_state.checkbox_indices[key] += 2
+            st.session_state.checkbox_indices[key] += max_checkbox
             st.experimental_rerun()
 
+
 def view_page():
-    keys = list(st.session_state.my_data.keys())[st.session_state.my_index:st.session_state.my_index + 2]
+    keys = list(st.session_state.my_data.keys())[st.session_state.my_index:st.session_state.my_index + max_expander]
     for key in keys:
         if key not in st.session_state.loaded_keys:
             st.session_state.loaded_keys.append(key)
-    st.session_state.my_index += 2
+    st.session_state.my_index += max_expander
+
+
+def update_index():
+    st.session_state.my_index = 0
+    st.session_state.loaded_keys = []
+    st.session_state.checkbox_indices = {}
+
+
+def update_sort():
+    st.session_state.my_data = dict(sorted(st.session_state.my_data.items(), 
+                                           key=lambda item: len(item[1]), 
+                                           reverse=st.session_state['sort_button']=='Desc'))
+    update_index()
+
 
 def base_component():
     st.text(f'Index: {st.session_state.my_index}')
     st.text(f'Data size: {st.session_state.length}')
+
+    op1 = st.selectbox('Please select In or Out of scope', options=['In', 'Out'], on_change=update_index)
+    op2 = st.selectbox('Please select sorting order', options=['Asc', 'Desc'], on_change=update_sort, key = 'sort_button')
+
+    sort_by_size(op1,desc= op2=='Desc')
+
 
     for key in st.session_state.loaded_keys:
         with st.expander(key):
@@ -65,6 +94,8 @@ def base_component():
                 view_page()
                 st.experimental_rerun()
 
+max_expander = 2
+max_checkbox = 2
 init_state()
 load_data()
 base_component()
